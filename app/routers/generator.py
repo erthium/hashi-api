2da0bbe07dc77becf158b7982b6a2a81b07fb4bf
Erthium <ertyum35@gmail.com>
2024-09-15 04:44:58 +0300

    feat: add a random generator route to generator

--- app/routers/generator.py
+++ app/routers/generator.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter, HTTPException, Request
 from typing import List
+from random import choice
 
 from app.libs.generator import generate_till_full
 from app.libs.utils import grid_to_string
 
 router = APIRouter()
+
+puzzle_sizes: list = [(5, 5), (10, 10), (15, 15), (20, 20), (25, 25)]
 
 @router.post(
   "/new",
@@ -16,5 +19,18 @@ router = APIRouter()
 async def calculate(width: int, height: int):
   if not width or not height:
     raise HTTPException(status_code=400, detail="Width and height are required")
+  new_puzzle = generate_till_full(width, height)
+  return grid_to_string(new_puzzle)
+
+
+@router.get(
+  "/random",
+  response_model=str,
+  summary="Generate a random puzzle",
+  description="Generates a random puzzle with a random width and height, and returns it as a string.",
+  response_description="The generated puzzle."
+)
+async def random():
+  width, height = choice(puzzle_sizes)
   new_puzzle = generate_till_full(width, height)
   return grid_to_string(new_puzzle)
